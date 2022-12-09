ae6b126f05c88bb5dde012fef7d1d65786b27332
sillypoise <somewhatjose@gmail.com>
2022-12-09 17:38:55 -0500

    dev: got vite setup working... I think

---
+++ apps/vite-app/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+    let test;
+
+    if (test) return <p>hello</p>;
+
+    return (
+        <div>
+            <h1>React App</h1>
+        </div>
+    );
+}
+
+export default App;
