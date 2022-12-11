b6783c0b8da50434ffbefd2f5926f03dc0103e9e
sillypoise <somewhatjose@gmail.com>
2022-12-11 12:20:09 -0500

    dev: think I got eslint working, what a pain in the arse

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "turbo",
+        "prettier",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ],
+};
