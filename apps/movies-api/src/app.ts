5860733f290bdf1afe05bf21e98e82e48b62c194
sillypoise <somewhatjose@gmail.com>
2022-12-11 16:06:13 -0500

    dev: got server and app connection working with monorepo setup! We should branch this off to consider later as a template candidate

--- apps/movies-api/src/app.ts
+++ apps/movies-api/src/app.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { api } from "./routes/api";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (_req: Request, res: Response) => {
     res.status(200).json("Congrats");
+});
+
+app.get("/health", (_req: Request, res: Response) => {
+    res.status(200).json("Healthy!");
 });
 
 app.use("/v1", api);
--- apps/vite-app/src/App.tsx
+++ apps/vite-app/src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
-    let test;
-
-    if (test) return <p>hello</p>;
+    const [data, setData] = useState("hello");
 
     return (
         <div>
             <h1>React App</h1>
+            <p>testing</p>
+            <p>{data}</p>
         </div>
     );
 }
