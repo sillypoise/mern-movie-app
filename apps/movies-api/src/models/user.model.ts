6c24101ee03d3f9b13da179a84e64eeffca5b5d7
sillypoise <somewhatjose@gmail.com>
2022-12-20 12:47:34 -0500

    dev: separated user collection into user and password collectin for security best practices

--- apps/movies-api/src/models/user.model.ts
+++ apps/movies-api/src/models/user.model.ts
@@ -1,28 +1,46 @@
+import { ClientSession } from "mongodb";
 import { mdb } from "../services/db/db";
 import {
+    IPasswordSchema,
     IUserWithPasswordSchema,
     userSchema,
     type IUserSchema,
 } from "../services/db/dbSchema";
 
 async function createUser(user: IUserWithPasswordSchema) {
+    let session: ClientSession | null = null;
     try {
         await mdb.connect();
-        const db = mdb.db("movie_app");
-        const userTable = db.collection<IUserWithPasswordSchema>("user");
-        const res = await userTable
-            .insertOne({
-                name: user.name,
-                email: user.email,
-                password: user.password,
-            })
-            .catch((err) => console.log(err));
-        if (res) {
-            console.log(`user inserted with _id:${res.insertedId} `); // eslint-disable-line
-        }
+        session = mdb.startSession();
+        await session.withTransaction(async () => {
+            const userTable = mdb
+                .db("movie_app")
+                .collection<IUserSchema>("user");
+            const passwordTable = mdb
+                .db("movie_app")
+                .collection<IPasswordSchema>("password");
+
+            const resUser = await userTable.insertOne({
+                name: "Test",
+                email: "test@example.com",
+                verified: false,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            });
+
+            if (resUser) {
+                console.log(`user inserted with _id:${resUser.insertedId} `); // eslint-disable-line
+
+                await passwordTable.insertOne({
+                    userId: resUser.insertedId,
+                    password: user.password,
+                });
+            }
+        });
     } catch (err) {
         console.dir(err);
     } finally {
+        if (session) await session.endSession();
         await mdb.close();
     }
 }
--- apps/movies-api/src/services/db/dbSchema.ts
+++ apps/movies-api/src/services/db/dbSchema.ts
@@ -1,23 +1,38 @@
+import { ObjectId } from "mongodb";
 import { z } from "zod";
 
 const userSchema = z.object({
     name: z.string(),
     email: z.string().email(),
     verified: z.boolean().default(false),
+    createdAt: z.date(),
+    updatedAt: z.date(),
 });
 
 const userWithPasswordSchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(8),
+    verified: z.boolean().default(false),
+});
+
+const passwordSchema = z.object({
+    userId: z.string(),
+    password: z.string().min(8),
 });
 
 type IUserSchema = z.infer<typeof userSchema>;
 type IUserWithPasswordSchema = z.infer<typeof userWithPasswordSchema>;
+type IPasswordSchema = {
+    userId: ObjectId;
+    password: string;
+};
 
 export {
     userSchema,
     IUserSchema,
     userWithPasswordSchema,
     IUserWithPasswordSchema,
+    passwordSchema,
+    IPasswordSchema,
 };
