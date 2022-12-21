669aa5eb697964c07bca3c1ac9386274b8e74bb7
sillypoise <somewhatjose@gmail.com>
2022-12-21 13:36:21 -0500

    dev: added middleware for commiting and requiring sessions

--- apps/movies-api/src/middleware/session.middleware.ts
+++ apps/movies-api/src/middleware/session.middleware.ts
@@ -1,10 +1,26 @@
 import { NextFunction, Request, Response } from "express";
-import { getSession } from "../services/sessions";
+import { commitSession, getSession } from "../services/sessions";
 
-async function createUserSession(userId: string, redirectTo: string) {
-    return (req: Request, res: Response, next: NextFunction) {
-    let session = await getSession();
-    session.set("userId", userId)
-    next()
-} 
+function createUserSession(userId: string) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        let session = await getSession();
+        session.set("userId", userId);
+        res.append("Set-Cookie", await commitSession(session));
+        next();
+    };
 }
+
+async function requireUserSession(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const session = await getSession(req.headers.cookie);
+    const userSession = session.get("userId");
+    if (!userSession) {
+        return res.redirect("/health");
+    }
+    next();
+}
+
+export { createUserSession, requireUserSession };
--- apps/movies-api/src/routes/user/user.router.ts
+++ apps/movies-api/src/routes/user/user.router.ts
@@ -1,19 +1,34 @@
 import express, { Request, Response } from "express";
 import { httpCreateUser } from "./user.controller";
+import {
+    createUserSession,
+    requireUserSession,
+} from "../../middleware/session.middleware";
+import { getSession } from "../../services/sessions";
 
 const userRouter = express.Router();
 
 userRouter.post("/create", httpCreateUser);
-userRouter.get("/login", async (req: Request, res: Response) => {
-    // console.log(req.headers.cookie);
+userRouter.get(
+    "/login",
+    createUserSession("123"),
+    async (req: Request, res: Response) => {
+        const session = await getSession(req.headers.cookie);
+        console.log(session.get("userId"));
 
-    let session = await getSession(req.headers.cookie);
+        // console.log(req.headers.cookie);
 
-    console.log(session.get("123"));
+        // let session = await getSession(req.headers.cookie);
 
-    session.set("123", "321");
-    res.append("Set-Cookie", await commitSession(session));
-    return res.json("login route");
+        // console.log(session.get("123"));
+
+        // session.set("123", "321");
+        // res.append("Set-Cookie", await commitSession(session));
+        return res.json("login route");
+    }
+);
+userRouter.get("/test", requireUserSession, (req, res) => {
+    return res.json("sup dude");
 });
 
 export { userRouter };
