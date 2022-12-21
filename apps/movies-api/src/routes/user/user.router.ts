c62f87366aa27efbfc6af32fb0cd6c3d73192624
sillypoise <somewhatjose@gmail.com>
2022-12-21 13:39:03 -0500

    dev: same session middleware abstractions but botched up git. stay put while we fix, maybe unnecessary commits incoming

--- apps/movies-api/src/routes/user/user.router.ts
+++ apps/movies-api/src/routes/user/user.router.ts
@@ -9,26 +9,8 @@ import { getSession } from "../../services/sessions";
 const userRouter = express.Router();
 
 userRouter.post("/create", httpCreateUser);
-userRouter.get(
-    "/login",
-    createUserSession("123"),
-    async (req: Request, res: Response) => {
-        const session = await getSession(req.headers.cookie);
-        console.log(session.get("userId"));
-
-        // console.log(req.headers.cookie);
-
-        // let session = await getSession(req.headers.cookie);
-
-        // console.log(session.get("123"));
-
-        // session.set("123", "321");
-        // res.append("Set-Cookie", await commitSession(session));
-        return res.json("login route");
-    }
-);
-userRouter.get("/test", requireUserSession, (req, res) => {
-    return res.json("sup dude");
+userRouter.get("/login", async (req: Request, res: Response) => {
+    return res.json("login route");
 });
 
 export { userRouter };
